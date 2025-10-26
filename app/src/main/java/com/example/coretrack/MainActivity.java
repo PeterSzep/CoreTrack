8acaabb232892a6532c34c257f0f1379082e36fd
ps119em <peter.szepesi@student.tuke.sk>
2025-10-26 16:11:28 +0100

    finished main activity design and started login activity design

--- app/src/main/java/com/example/coretrack/MainActivity.java
+++ app/src/main/java/com/example/coretrack/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.coretrack;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
+
 public class MainActivity extends AppCompatActivity {
+    private CircularProgressBar stepsProgressBar;
+    private TextView stepsTextview, caloriesTextView, kilometersTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        stepsProgressBar = findViewById(R.id.stepCircularBar);
+        stepsTextview = findViewById(R.id.stepsTextview);
+        caloriesTextView = findViewById(R.id.caloriesTextView);
+        kilometersTextView = findViewById(R.id.kilometersTextView);
+
+        stepsProgressBar.setProgressMax(10000f);
+        String currentSteps = "7500";
+        stepsProgressBar.setProgress(Float.parseFloat(currentSteps));
+
+        caloriesTextView.setText("500\nKcal");
+        kilometersTextView.setText("5,5\nKm");
+        stepsTextview.setText(currentSteps + "\nsteps");
     }
 }
